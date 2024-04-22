40be2c64fba7f6ef9d785b40741ad3aab1e01239
Rick Weber <863114+RickWeber@users.noreply.github.com>
2024-04-22 15:47:36 -0400

    Get Shiny app working with water tank dataset.

--- tanks/app.R
+++ tanks/app.R
@@ -13,16 +13,15 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Water tank prices"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+          sliderInput("min_size", "Minimum capacity", min = 10, max = 100, value = 20),
+          sliderInput("max_size", "Maximum capacity", min = 101, max = 250, value = 100),
+          numericInput("cap_hat", "hypothetical capacity", value = 50),
+          textOutput("predPrice")
         ),
 
         # Show a plot of the generated distribution
@@ -34,16 +33,25 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+  
+  df <- reactive({read_csv('../tank_prices.csv') %>% 
+     filter(capacity >= input$min_size, capacity <= input$max_size)})
+  model <- reactive({lm(price ~ capacity, data=df())})
+  output$predPrice <- renderText({
+    paste0(
+      "expected price: $", 
+      round(
+    predict(model(), newdata = data.frame(capacity = input$cap_hat)) , digits = 2
+      )
+    )
+  })
+  output$distPlot <- renderPlot({
+      #ggplot(df, aes(capacity, price, alpha=gal_per_dol, color=log(floor_space))) +
+      ggplot(df(), aes(capacity, price, color=log(floor_space))) +
+        geom_point() +
+        geom_vline(aes(xintercept = input$cap_hat)) +
+        geom_hline(aes(yintercept = predict(model(), newdata = data.frame(capacity = input$cap_hat)))) +
+        geom_smooth(method="lm")
     })
 }
 
