6b2f45ecf954934a76ca9f0a2fdd7f971f8ed1b1
Rick Weber <863114+RickWeber@users.noreply.github.com>
2024-04-23 09:21:17 -0400

    start to replicate minutefocus.com

---
+++ minute_focus.R
@@ -0,0 +1,22 @@
+rm(list=ls())
+library(tidyverse)
+library(gganimate)
+
+df <- tibble(
+  time = 1:11,
+  size = c(100,100,100,75,50,50,50,50,50,75,100)
+)
+
+initial_plot <- ggplot(data.frame(x=c(0,100), y=c(100,0)))
+plot <- initial_plot + geom_point(aes(size=size, x=50, y=50), data=df) +
+  transition_states(time) +
+  ease_aes('sine-in-out')
+
+animate(plot)
+anim_save('test.gif', animate(plot))
+
+# TODO
+# * fix timing (make animation slower?)
+# * get rid of background
+# * label with time? 
+
