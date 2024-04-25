d76b85fdebdad76c2f31012b29a7121c28bb3c41
Rick Weber <863114+RickWeber@users.noreply.github.com>
2024-04-25 08:44:14 -0400

    eliminate legend and background

--- minute_focus.R
+++ minute_focus.R
@@ -10,6 +10,8 @@ df <- tibble(
 initial_plot <- ggplot(data.frame(x=c(0,100), y=c(100,0)))
 plot <- initial_plot + geom_point(aes(size=size, x=50, y=50), data=df) +
   transition_states(time) +
+  theme_void() +
+  theme(legend.position = "none") +
   ease_aes('sine-in-out')
 
 animate(plot)
@@ -17,6 +19,5 @@ anim_save('test.gif', animate(plot))
 
 # TODO
 # * fix timing (make animation slower?)
-# * get rid of background
 # * label with time? 
 
